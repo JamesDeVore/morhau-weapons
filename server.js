c9def6a9b5fcd5560f3dc36ef1e3d31895b07d3a
James DeVore <jdevore4592@gmail.com>
2019-07-23 22:13:16 -0400

    Initial commit from Create React App

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+app.use(cors());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+//routes
+
+app.use(bodyParser.json());
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, "client/build")));
+
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+}
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on ${port}!`));
